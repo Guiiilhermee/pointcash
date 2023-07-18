d1d9c5c7e32451c449e2e7a3d42367f2861b83f9
Guiiilhermee <>
2023-07-18 22:28:16 +0000

    Code passed and fixed through a linter(PP8)

--- run.py
+++ run.py
@@ -12,24 +12,25 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('cashpoint')
 
+
 class atm_machine:
     def __init__(self, account_number, pin):
         self.account_number = account_number
         self.pin = pin
         self.balance = 1500
 
-        def deposit(self, balance):
-        self.balance += balance
-        print(f"Successfully done! €{balance:.2f} Balance: €{self.balance:.2f}")
+    def deposit(self, balance):
+        self.deposit += balance
+        print(f"Successfully done! €{balance:.2f} Balance:€{self.balance:.2f}")
 
-        def withdraw(self, balance):
+    def withdraw(self, balance):
         if self.balance >= balance:
             self.balance -= balance
-            print(f"Successfully done! €{balance:.2f} Balance: €{self.balance:.2f}")
+        print(f"Successfully done! €{balance:.2f} Balance:€{self.balance:.2f}")
         else:
             print("Insufficient balance.")
 
-        def transfer(self, balance, destination_account):
+    def transfer(self, balance, destination_account):
         if self.balance >= balance:
             self.balance -= balance
             destination_account.balance += balance
@@ -38,13 +39,14 @@ class atm_machine:
         else:
             print("Insufficient balance for transfer.")
 
-        def options(self):
-            print("Options:")
-            print("1. Deposit")
-            print("2. Withdraw")
-            print("3. Transfer")
-            print("4. Check balance")
-            print("0. Exit")
+    def options(self):
+        print("Options:")
+        print("1. Deposit")
+        print("2. Withdraw")
+        print("3. Transfer")
+        print("4. Check balance")
+        print("0. Exit")
+
 
 account_number = input("Enter your account number:\n ")
 pin = input("Enter your PIN: ")
@@ -61,7 +63,7 @@ while True:
         balance = float(input("Enter the deposit amount:\n €"))
         atm.deposit(balance)
 
-     elif options == "2":
+    elif options == "2":
         balance = float(input("Enter the withdraw amount:\n €"))
         atm.withdraw(balance)
 
