afb53da093c991a9c0ce8ef4ed3aaf6b3e8dcb6f
Guiiilhermee <>
2023-07-18 20:41:35 +0000

    Add class atm machine

--- run.py
+++ run.py
@@ -1,20 +1,19 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('cashpoint')
 
-sales = SHEET.worksheet('sales')
-
-data = sales.get_all_values()
-
-print(data)
+class atm_machine:
+    def __init__(self, account_number, pin):
+        self.account_number = account_number
+        self.pin = pin
+        self.balance = 1500
